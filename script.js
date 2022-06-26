2c093bc72e90cc8bc04bb235990dee40131c814e
SARANSH CHAUHAN <66305908+Saransh-Chauhan@users.noreply.github.com>
2022-06-26 17:42:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function hideMenu() {
     navLinks.style.right = "-200px";
 }
 // navbar togglemenu end
+
+const questions = document.querySelectorAll('.question-answer');
+
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener("click", function() {
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        })
+        question.classList.toggle("show-text");
+    })
+})
